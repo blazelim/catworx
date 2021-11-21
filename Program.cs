1f473988524a2cf56d3503420ebff77a04c8baa9
Blaze Lim <blazebentleycolim@gmail.com>
2021-11-21 15:19:57 -0800

    initial files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static void Main(string[] args) // entry point
+    {   
+        List<string> employees = new List<string>() { "adam", "amy" };
+        employees.Add("barbara");
+        employees.Add("billy");
+
+        for (int i = 0; i < employees.Count; i++) 
+        {
+        Console.WriteLine(employees[i]);
+        }
+    }
+  }
+}
