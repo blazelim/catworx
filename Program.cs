2b6d3f8f9b46de041120f2ae75d485bfba3948df
Blaze Lim <blazebentleycolim@gmail.com>
2021-11-22 18:53:29 -0800

    added getemployees and print employees functions

--- Program.cs
+++ Program.cs
@@ -6,16 +6,38 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-    static void Main(string[] args) // entry point
-    {   
-        List<string> employees = new List<string>() { "adam", "amy" };
-        employees.Add("barbara");
-        employees.Add("billy");
 
-        for (int i = 0; i < employees.Count; i++) 
+    static List<string> GetEmployees()
+    {
+      // I will return a lost of strings
+      List<string> employees = new List<string>();
+      while (true)
+      {        
+        Console.WriteLine("Please enter a name: ");
+        string input = Console.ReadLine();
+        // Break if the user hits ENTER without typing a name
+        if (input == "")
         {
-        Console.WriteLine(employees[i]);
+          break;
         }
+        employees.Add(input);
+      }
+      return employees;
+    }
+
+    static void PrintEmployees(List<string> employees)
+    {
+      for (int i = 0; i<employees.Count; i++)
+      {
+        Console.WriteLine(employees[i]);
+      }
+    }
+
+        static void Main(string[] args) // entry point
+    {   
+      // this is our employee getting code now
+      List<string> employees = GetEmployees();
+      PrintEmployees(employees);
     }
   }
 }
