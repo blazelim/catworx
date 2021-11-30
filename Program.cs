8f955a1b20de7438637cdfa097c19862cd8bd882
Blaze Lim <blazebentleycolim@gmail.com>
2021-11-29 20:00:23 -0800

    added employee prompts

--- Program.cs
+++ Program.cs
@@ -7,36 +7,49 @@ namespace CatWorx.BadgeMaker
   class Program
   {
 
-    static List<string> GetEmployees()
+    static List<Employee> GetEmployees()
     {
       // I will return a lost of strings
-      List<string> employees = new List<string>();
+      List<Employee> employees = new List<Employee>();
       while (true)
       {        
-        Console.WriteLine("Please enter a name: ");
-        string input = Console.ReadLine();
+        Console.WriteLine("Enter First Name: (leave empty to exit)");
+        string firstName = Console.ReadLine();
         // Break if the user hits ENTER without typing a name
-        if (input == "")
+        if (firstName == "")
         {
           break;
         }
-        employees.Add(input);
+
+        // add a Console.ReadLine() for each value
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+        Console.Write("Enter ID: ");
+        int id = Int32.Parse(Console.ReadLine());
+        Console.Write("Enter Photo URL:");
+        string photoUrl = Console.ReadLine();
+        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+        
+        // add current employee not a string
+        employees.Add(currentEmployee);
       }
+
       return employees;
     }
 
-    static void PrintEmployees(List<string> employees)
+    static void PrintEmployees(List<Employee> employees)
     {
-      for (int i = 0; i<employees.Count; i++)
+      for (int i = 0; i < employees.Count; i++) 
       {
-        Console.WriteLine(employees[i]);
+        string template = "{0,-10}\t{1,-20}\t{2}";
+        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
       }
     }
 
-        static void Main(string[] args) // entry point
+    static void Main(string[] args) // entry point
     {   
       // this is our employee getting code now
-      List<string> employees = GetEmployees();
+      List<Employee> employees = GetEmployees();
       PrintEmployees(employees);
     }
   }
