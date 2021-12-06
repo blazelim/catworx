9bb44794672a05975fa4e7de1020033e21094314
Blaze Lim <blazebentleycolim@gmail.com>
2021-12-05 21:49:18 -0800

    added generator for employees

--- Program.cs
+++ Program.cs
@@ -2,40 +2,13 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace CatWorx.BadgeMaker
 {
   class Program
   {
 
-    static List<Employee> GetEmployees()
-    {
-      // I will return a lost of strings
-      List<Employee> employees = new List<Employee>();
-      while (true)
-      {        
-        Console.WriteLine("Enter First Name: (leave empty to exit)");
-        string firstName = Console.ReadLine();
-        // Break if the user hits ENTER without typing a name
-        if (firstName == "")
-        {
-          break;
-        }
 
-        // add a Console.ReadLine() for each value
-        Console.Write("Enter last name: ");
-        string lastName = Console.ReadLine();
-        Console.Write("Enter ID: ");
-        int id = Int32.Parse(Console.ReadLine());
-        Console.Write("Enter Photo URL:");
-        string photoUrl = Console.ReadLine();
-        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-        
-        // add current employee not a string
-        employees.Add(currentEmployee);
-      }
-
-      return employees;
-    }
 
     static void Main(string[] args) // entry point
     {   
@@ -45,7 +18,8 @@ namespace CatWorx.BadgeMaker
       // Util.MakeCSV(employees);
 
         List<Employee> employees = new List<Employee>();
-        employees = GetEmployees();
+        // employees = GetEmployees();
+        employees = PeopleFetcher.GetFromApi();
         Util.MakeCSV(employees);
         Util.MakeBadges(employees);
     }
